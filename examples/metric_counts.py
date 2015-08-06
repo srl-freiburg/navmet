370e56b0c8e0dc78450eeab37647190fc55063ec
Billy Okal <okal.billy@googlemail.com>
2015-08-06 15:41:46 +0200

    adding basic tests

--- examples/metric_counts.py
+++ examples/metric_counts.py
@@ -1,19 +1,13 @@
 
 
-import os
-import sys
-# local devel testing without installing the package
-sys.path = [os.path.abspath(os.path.join(os.getcwd(), '..')), ] + sys.path
-
-
 import navmet
 import numpy as np
+
 import matplotlib.pyplot as plt
-plt.style.use('ggplot')
+plt.style.use('fivethirtyeight')
 
 from matplotlib.patches import Ellipse, Circle
 
-## Subjective metrics
 # load some sample agents
 agents = np.loadtxt('sample_agents.txt')
 
@@ -21,34 +15,43 @@ agents = np.loadtxt('sample_agents.txt')
 focal_agent = agents[0, :]
 other_agents = agents[1:, :]
 
+# -- Subjective metrics
 ic, pc, sc = navmet.count_uniform_intrusions(focal_agent, other_agents)
-print('Instrusion Counts: Intimate = {}, Personal = {}, Social = {}'.format(ic, pc, sc))
+print('Instrusion Counts: Intimate = {}, Personal = {}, Social = {}'
+      .format(ic, pc, sc))
 
-
-## Objective metrics
+# -- Objective metrics
 traj = np.loadtxt('sample_traj.txt')
 pl = navmet.path_length(trajectory=traj)
 chc = navmet.cumulative_heading_changes(trajectory=traj)
-print('Objective Metrics: Path length = {}, Cum. heading changes = {}'.format(pl, chc))
+print('Objective Metrics: Path length = {}, Cum. heading changes = {}'
+      .format(pl, chc))
 
 
 # plot their configuration
 plt.figure(figsize=(8, 6))
-plt.plot(agents[:, 0], agents[:, 1], ls='', marker='8', markersize=15, label='Sample agents')
+plt.plot(agents[:, 0], agents[:, 1], ls='', marker='8', markersize=15,
+         label='Sample agents')
 plt.plot(traj[:, 0], traj[:, 1], ls='-', lw=1.5, label='Sample trajectory')
 plt.legend(loc='best', numpoints=1)
 
 # zooming in to the context
 plt.figure(figsize=(8, 8))
 ax = plt.subplot2grid((1, 1), (0, 0), rowspan=1, colspan=1)
-ax.plot(0.0, 0.0, marker='8', markersize=20, color='r', linestyle='', label='Focal Agent')
-ax.add_artist(Circle((0.0, 0.0), radius=3.6, color='r', fill=False, hatch='.', alpha=0.3, lw=2.))
+ax.plot(0.0, 0.0, marker='8', markersize=20, color='r', linestyle='',
+        label='Focal Agent')
+ax.add_artist(Circle((0.0, 0.0), radius=3.6, color='r', fill=False, hatch='.',
+              alpha=0.3, lw=2.))
 
 for n in agents:
     heading = np.degrees(np.arctan2(n[3], n[2]))
-    ax.add_artist(Ellipse((n[0] - focal_agent[0], n[1] - focal_agent[1]), width=0.3, height=0.8, angle=heading, color='b', fill=False, lw=1.5))
-    ax.add_artist(Circle((n[0] - focal_agent[0], n[1] - focal_agent[1]), radius=0.2, color='b'))
-    ax.arrow(n[0] - focal_agent[0], n[1] - focal_agent[1], n[2], n[3], fc='b', ec='b', head_width=0.1, head_length=0.1)
+    ax.add_artist(Ellipse((n[0] - focal_agent[0], n[1] - focal_agent[1]),
+                  width=0.3, height=0.8, angle=heading, color='b',
+                  fill=False, lw=1.5))
+    ax.add_artist(Circle((n[0] - focal_agent[0], n[1] - focal_agent[1]),
+                  radius=0.2, color='b'))
+    ax.arrow(n[0] - focal_agent[0], n[1] - focal_agent[1], n[2], n[3], fc='b',
+             ec='b', head_width=0.1, head_length=0.1)
 
 plt.axis('equal')
 ax.set_xlim([-6, 6])
--- navmet/tests/test_angles_utils.py
+++ navmet/tests/test_angles_utils.py
@@ -1,5 +1,4 @@
 
-from numpy.testing import assert_approx_equal
 from numpy.testing import assert_equal
 
 import numpy as np
@@ -10,6 +9,25 @@ from navmet.angle_utils import subangles
 
 
 def test_norm_angle():
-    a1 = np.pi
-    a2 = 3*np.pi
-    assert_equal(normalize(a2), a1)
+    assert_equal(normalize(3*np.pi), np.pi)
+    assert_equal(normalize(np.pi/2.0), np.pi/2.0)
+    assert_equal(normalize(-np.pi), np.pi)
+    assert_equal(normalize(5*np.pi), np.pi)
+
+
+def test_and_angles():
+    # pi + pi = 0 or 2pi
+    assert_equal(addangles(np.pi, np.pi), 0.0)
+    # 2pi + pi = pi
+    assert_equal(addangles(2*np.pi, np.pi), np.pi)
+    # pi + -pi/2 = pi/2
+    assert_equal(addangles(np.pi, -np.pi/2.0), np.pi/2.0)
+
+
+def test_sub_angles():
+    # pi - 2pi = pi
+    assert_equal(subangles(np.pi, 2*np.pi), np.pi)
+    # 2pi - pi = pi
+    assert_equal(subangles(2*np.pi, np.pi), np.pi)
+    # pi - -pi/2 = pi/2
+    assert_equal(subangles(np.pi, -np.pi/2.0), np.pi/2.0)
